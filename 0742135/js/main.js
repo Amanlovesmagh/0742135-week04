01419b159a4c93b419e12ac77cc1814ed879545a
visualstudio <visualstudio@230-PC31>
2019-01-31 11:46:41 -0500

    i created function

--- 0742135/js/main.js
+++ 0742135/js/main.js
@@ -1,1 +1,8 @@
-﻿// Your code here!
+﻿var date = new Date();
+
+
+
+function myFunction() {
+    
+    document.body.innerHTML = "<h1> The date is " + (date.getMonth() + 1) + " / " + date.getDate() + "/" + date.getFullYear() + "</h1>"
+}
